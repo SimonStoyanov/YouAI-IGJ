a42862ebc52fb95462d9b4ce3256f9ae3d9e1a84
Simón Stoyanov Beltrán <simon.stoyanov@hotmail.com>
2018-11-25 12:04:32 +0100

    added Capsule button

---
+++ YouAI-IGJ/Assets/Scripts/MonkeyAnimationManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkeyAnimationManager : MonoBehaviour {
+
+    public Animator monkey;
+    public Animator chair;
+    public Video video;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool has_video = video.sprite_in_use != null ? true : false;
+        monkey.SetBool("has_video", has_video);
+        chair.SetBool("has_video", has_video);
+    }
+}
